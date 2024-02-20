f4f2d4e9c24b229b9af6dd0d0e2418f3b7f2e88b
Christopher <christopher.hsc2520@gmail.com>
2024-02-20 17:35:31 +0100

    feature: now the whole CRUD works on prompts

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
 import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
@@ -12,24 +13,20 @@ const handler = NextAuth({
   ],
   callbacks: {
     async session({ session }) {
-      try {
-        await connectToDB();
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-        const sessionUser = await User.findOne({ email: session.user.email });
-        session.user.id = sessionUser?._id.toString();
-
-        return session;
-      } catch (error) {
-        console.error("Error in session callback: ", error.message);
-        throw new Error('Failed to retrieve session');
-      }
+      return session;
     },
     async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
 
+        // check if user already exists
         const userExists = await User.findOne({ email: profile.email });
 
+        // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
             email: profile.email,
@@ -38,13 +35,13 @@ const handler = NextAuth({
           });
         }
 
-        return true;
+        return true
       } catch (error) {
-        console.error("Error checking if user exists: ", error.message);
-        return false;
+        console.log("Error checking if user exists: ", error.message);
+        return false
       }
     },
   }
-});
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
--- utils/database.js
+++ utils/database.js
@@ -4,15 +4,14 @@ let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
   if (mongoose.connection.readyState === 1) {
-    console.log('MongoDB is already connected');
     return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('Failed to connect to MongoDB:', error);
+    console.error("Failed to connect to MongoDB:", error);
     throw error;
   }
 
